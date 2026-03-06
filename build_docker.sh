77150b3a8ff665bb0e2bfebb7eb44eafe31bb48a
Ákos Madaras <akosmadaras88@gmail.com>
2026-03-06 10:19:24 +0100

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -3,5 +3,6 @@
 # Export variable for building the image
 HOST_USER_GROUP_ARG=$(id -g $USER)
 docker build .\
+    --no-cache \
     --tag sgwrs:latest \
     --build-arg HOST_USER_GROUP_ARG=$HOST_USER_GROUP_ARG
